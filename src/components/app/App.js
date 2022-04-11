ea22b4e65d9a669c7aa47473e85ccf8b15f409f8
Benjamin Lamboy <blamboy@catalyte.io>
2022-04-11 15:49:22 -0600

    test toast code written not currently functional

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import ProductPage from '../product-page/ProductPage';
-import CheckoutPage from '../checkout-page/CheckoutPage';
-import ConfirmationPage from '../confirmation-page/ConfirmationPage';
-import Header from '../header/Header';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import ProductPage from "../product-page/ProductPage";
+import CheckoutPage from "../checkout-page/CheckoutPage";
+import ConfirmationPage from "../confirmation-page/ConfirmationPage";
+import Header from "../header/Header";
+import Toast from "./component/toast/Toast";
+import checkIcon from './assets/check.svg';
+import errorIcon from './assets/error.svg';
+import infoIcon from './assets/info.svg';
+import warningIcon from './assets/warning.svg';
 
 /**
  * @name App
  * @returns component
  */
-const App = () => (
+const App = () => {
   <BrowserRouter>
     <Header />
     <Switch>
@@ -18,7 +23,35 @@ const App = () => (
       <Route exact path="/checkout" render={() => <CheckoutPage />} />
       <Route exact path="/confirmation" render={() => <ConfirmationPage />} />
     </Switch>
-  </BrowserRouter>
-);
+  </BrowserRouter>;
+  const testList = [
+    {
+      id: 1,
+      title: 'Success',
+      description: 'This is a success toast component',
+      backgroundColor: '#5cb85c',
+      icon: checkIcon
+    },
+    {
+      id: 2,
+      title: 'Danger',
+      description: 'This is an error toast component',
+      backgroundColor: '#d9534f',
+      icon: errorIcon
+    },
+  ];
+
+  return (
+    <div className="app">
+      <div className="app-header"></div>
+      <Toast
+        toastList={testList}
+        position="bottom-right"
+      />
+    </div>
+  );
+  
+};
+
 
 export default App;
