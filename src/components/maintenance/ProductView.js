0afad9455ad146e45b97e81d6bb19ddcd3428590
Victoria Shelton <vshelton@catalyte.io>
2022-04-12 17:26:47 -0400

    add fetch to send get data request to api

--- src/components/maintenance/ProductView.js
+++ src/components/maintenance/ProductView.js
@@ -1,12 +1,24 @@
 import React, { useEffect, useState } from 'react';
+import styles from './ProductView.module.css';
 import Constants from '../../utils/constants';
+import fetchProducts from './ProductViewService';
 
+/**
+ * @name ProductView
+ * @description fetches products from API and displays products in a table
+ * @return component
+ */
 const ProductView = () => {
     const [products, setProducts] = useState([]);
     const [apiError, setApiError] = useState(false);
-  
+
+    useEffect(() => {
+        fetchProducts(setProducts, setApiError);
+    }, []);
+
     return (
         <div>
+            {apiError && <p className={styles.errMsg} data-testid="errMsg">{Constants.API_ERROR}</p>}
             <h1>Maintenance Page</h1>
         </div>
     );
