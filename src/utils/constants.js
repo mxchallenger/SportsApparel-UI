67cfabba4d661e11f91bb607f882b7c5f651e789
Julius Amaker <jamaker@catalyte.io>
2022-04-12 15:19:01 -0700

    Added active products endpoint to constant.js. Replaced property in HttpHelper function in ProductPageService.js. Active products display when page loads.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,6 +3,6 @@ module.exports = Object.freeze({
   BASE_URL_API: 'http://localhost:8085',
   PLACEHOLDER_IMAGE: 'https://www.signfix.com.au/wp-content/uploads/2017/09/placeholder-600x400.png',
   PURCHASE_ENDPOINT: '/purchases',
-  RUNNING_SHORTS_ENDPOINT: '/products?category=Running&type=Short',
+  PRODUCTS_ACTIVE_ENDPOINT: '/products/active',
   GOOGLE_CLIENT_ID: '912899852587-7996nh9mlpvpa2446q0il4f9hj5o492h.apps.googleusercontent.com' // ENTER CLIENT ID HERE
 });
