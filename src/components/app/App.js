8cde142d2eda903375478cf295b084112fdead73
Benjamin Lamboy <blamboy@catalyte.io>
2022-04-14 13:24:18 -0600

    removed custom toast and added toast via library

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import ProductPage from '../product-page/ProductPage';
 import CheckoutPage from '../checkout-page/CheckoutPage';
 import ConfirmationPage from '../confirmation-page/ConfirmationPage';
-import ToastDemoPage from '../toast/ToastDemoPage';
 import Header from '../header/Header';
+import Toast from '../toast/Toast';
 
 /**
  * @name App
@@ -18,9 +20,11 @@ const App = () => (
       <Route exact path="/" render={() => <ProductPage />} />
       <Route exact path="/checkout" render={() => <CheckoutPage />} />
       <Route exact path="/confirmation" render={() => <ConfirmationPage />} />
-      <Route exact path="/toastdemo" render={() => <ToastDemoPage />} />
+      <Route exact path="/toast" render={() => <Toast />} />
+      <ToastContainer />
     </Switch>
   </BrowserRouter>
+
 );
 
 export default App;
--- src/components/toast/Toast.js
+++ src/components/toast/Toast.js
@@ -1,76 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import './Toast.css';
+import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
 
-const Toast = (props) => {
-  const {
-    toastList, position, autoDelete, autoDeleteTime
-  } = props;
-  const [list, setList] = useState(toastList);
+import 'react-toastify/dist/ReactToastify.css';
+// minified version is also included
+// import 'react-toastify/dist/ReactToastify.min.css';
 
-  useEffect(() => {
-    setList(toastList);
-  }, [toastList, list]);
-
-  const deleteToast = (id) => {
-    const listItemIndex = list.findIndex((e) => e.id === id);
-    const toastListItem = toastList.findIndex((e) => e.id === id);
-    list.splice(listItemIndex, 1);
-    list.splice(toastListItem, 1);
-    setList([...list]);
-  };
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (autoDelete && toastList.length && list.length) {
-        deleteToast(toastList[0].id);
-      }
-    }, autoDeleteTime);
-    return () => {
-      clearInterval(interval);
-    };
-  });
+function toasty() {
+  const notify = () => toast('You have been notified.');
 
   return (
-    <>
-      <div className={`notification-container ${position}`}>
-        {
-                  list.map((toast, listIndex) => (
-                    <div
-                      key={listIndex.id}
-                      className={`notification toast ${position}`}
-                      style={{ backgroundColor: toast.backgroundColor }}
-                    >
-                      <button type="button" onClick={() => deleteToast(toast.id)}>
-                        X
-                      </button>
-                      <div className="notification-image">
-                        <img src={toast.icon} alt="" />
-                      </div>
-                      <div>
-                        <p className="notification-title">{toast.title}</p>
-                        <p className="notification-message">
-                          {toast.description}
-                        </p>
-                      </div>
-                    </div>
-                  ))
-              }
-      </div>
-    </>
+    <div>
+      <button type="button" onClick={notify}>Notify!</button>
+      <ToastContainer
+        position="top-center"
+        autoClose={8000}
+      />
+    </div>
   );
-};
+}
 
-Toast.defaultProps = {
-  position: 'centered',
-  autoDelete: true,
-  autoDeleteTime: 8000
-};
-
-Toast.propTypes = {
-  toastList: PropTypes.arrayOf.isRequired,
-  position: PropTypes.string,
-  autoDelete: PropTypes.bool,
-  autoDeleteTime: PropTypes.number
-};
-export default Toast;
+export default toasty;
