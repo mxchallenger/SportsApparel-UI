e21c5c23515000e7350bd431f9c94af53a0e99d3
Victoria Shelton <vshelton@catalyte.io>
2022-04-15 16:57:56 -0400

    use predefined fetch

--- src/components/maintenance/Maintenance.js
+++ src/components/maintenance/Maintenance.js
@@ -2,7 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
-import { BASE_URL_API, PRODUCT_ENDPOINT } from '../../utils/constants';
+import fetchProducts from './MaintenanceService';
+import styles from './Maintenance.module.css';
+import Constants from '../../utils/constants';
+
 /**
  * @name Maintenance
  * @description fetches products from API and displays products in a table
@@ -11,34 +14,34 @@ import { BASE_URL_API, PRODUCT_ENDPOINT } from '../../utils/constants';
 const Maintenance = () => {
   const [columnDefs] = useState([
     { field: 'id', sortable: true },
-    { field: 'name', sortable: true, suppressSizeToFit: true },
-    { field: 'description', sortable: true, suppressSizeToFit: true },
-    { field: 'price', sortable: true, suppressSizeToFit: true },
-    { field: 'quantity', sortable: true, suppressSizeToFit: true },
-    { field: 'active', sortable: true, suppressSizeToFit: true },
-    { field: 'category', sortable: true, suppressSizeToFit: true },
-    { field: 'type', sortable: true, suppressSizeToFit: true },
-    { field: 'brand', sortable: true, suppressSizeToFit: true },
-    { field: 'material', sortable: true, suppressSizeToFit: true },
-    { field: 'sku', sortable: true, suppressSizeToFit: true },
-    { field: 'globalProductCode', sortable: true, suppressSizeToFit: true },
-    { field: 'releaseDate', sortable: true, suppressSizeToFit: true },
-    { field: 'imageSrc', sortable: true, suppressSizeToFit: true },
-    { field: 'primaryColorCode', sortable: true, suppressSizeToFit: true },
-    { field: 'secondaryColorCode', sortable: true, suppressSizeToFit: true },
-    { field: 'demographic', sortable: true, suppressSizeToFit: true }
+    { field: 'name', sortable: true },
+    { field: 'description', sortable: true },
+    { field: 'price', sortable: true },
+    { field: 'quantity', sortable: true },
+    { field: 'active', sortable: true },
+    { field: 'category', sortable: true },
+    { field: 'type', sortable: true },
+    { field: 'brand', sortable: true },
+    { field: 'material', sortable: true },
+    { field: 'sku', sortable: true },
+    { field: 'globalProductCode', sortable: true },
+    { field: 'releaseDate', sortable: true },
+    { field: 'imageSrc', sortable: true },
+    { field: 'primaryColorCode', sortable: true },
+    { field: 'secondaryColorCode', sortable: true },
+    { field: 'demographic', sortable: true }
   ]);
 
   const [rowData, setRowData] = useState([]);
+  const [apiError, setApiError] = useState(false);
 
   useEffect(() => {
-    fetch(BASE_URL_API + PRODUCT_ENDPOINT)
-      .then((response) => response.json())
-      .then((data) => setRowData(data));
+    fetchProducts(setRowData, setApiError);
   }, []);
 
   return (
     <div className="scroll">
+      {apiError && <p className={styles.errMsg} data-testid="errMsg">{Constants.API_ERROR}</p>}
       <div className="ag-theme-alpine" style={{ height: 450, width: '100%', columnFill: 'balance-all' }}>
         <AgGridReact
           rowData={rowData}
