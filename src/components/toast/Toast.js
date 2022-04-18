f8b5ff7c05c0b7a8682759e34cacd16e303f6286
Benjamin Lamboy <blamboy@catalyte.io>
2022-04-18 09:27:34 -0600

    removed commented code

--- src/components/toast/Toast.js
+++ src/components/toast/Toast.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { toast } from 'react-toastify';
 
 import 'react-toastify/dist/ReactToastify.css';
-// minified version is also included
-// import 'react-toastify/dist/ReactToastify.min.css';
 
 const toasty = () => (
 
