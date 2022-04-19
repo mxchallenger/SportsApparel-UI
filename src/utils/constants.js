439c19c598ccca239395ea56ccd19b38e72b8d9b
Dany Plugge <dplugge@catalyte.io>
2022-04-19 10:05:24 -0400

    Heroku endpoint

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 module.exports = Object.freeze({
   API_ERROR: 'Oops, something went wrong',
-  BASE_URL_API: 'http://localhost:8085',
+  // BASE_URL_API: 'http://localhost:8085',
+  BASE_URL_API: 'https://all-american-regex-ui.herokuapp.com/',
   PLACEHOLDER_IMAGE: 'https://www.signfix.com.au/wp-content/uploads/2017/09/placeholder-600x400.png',
   PURCHASE_ENDPOINT: '/purchases',
   RUNNING_SHORTS_ENDPOINT: '/products?category=Running&type=Short',
