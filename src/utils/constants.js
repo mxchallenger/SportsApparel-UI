2cc2b4574c3e25d0f69d6521fc13212eca810389
Benjamin Lamboy <blamboy@catalyte.io>
2022-04-26 09:32:52 -0600

    updated google client id

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,6 +5,6 @@ module.exports = Object.freeze({
   PLACEHOLDER_IMAGE: 'https://www.signfix.com.au/wp-content/uploads/2017/09/placeholder-600x400.png',
   PURCHASE_ENDPOINT: '/purchases',
   PRODUCTS_ACTIVE_ENDPOINT: '/products/active',
-  GOOGLE_CLIENT_ID: '912899852587-7996nh9mlpvpa2446q0il4f9hj5o492h.apps.googleusercontent.com', // ENTER CLIENT ID HERE
+  GOOGLE_CLIENT_ID: '518491329427-8ig1j2q680uoghj13uqkq7efbitkvle6.apps.googleusercontent.com', // ENTER CLIENT ID HERE
   PRODUCT_ENDPOINT: '/products'
 });
