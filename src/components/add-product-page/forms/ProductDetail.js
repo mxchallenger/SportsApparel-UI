bbbdee7236ffcf1113fb1ff7003e0efc08722fea
Victoria Shelton <vshelton@catalyte.io>
2022-04-29 14:39:12 -0400

    rearrange form items for 2 columns instead of 3

--- src/components/add-product-page/forms/ProductDetail.js
+++ src/components/add-product-page/forms/ProductDetail.js
@@ -33,138 +33,170 @@ const ProductDetail = ({ onChange, productData }) => {
 
   return (
     <div className={styles.productDetailContainer}>
-      <FormItem
-        placeholder="e.g. Soft Golf Shirt "
-        type="text"
-        id="name"
-        label="Product Name"
-        onChange={onChange}
-        value={productData.name}
-      />
-      <FormItem
-        type="text"
-        id="category"
-        label="Category"
-        onChange={onChange}
-        value={productData.category}
-      />
-      <FormItem
-        type="text"
-        id="imageSrc"
-        label="Image Source"
-        onChange={onChange}
-        value={productData.imageSrc}
-      />
-      <FormItem
-        placeholder="e.g. Golf, Men, Soft"
-        type="text"
-        id="description"
-        label="Product Description"
-        onChange={onChange}
-        value={productData.description}
-      />
-      <FormItem
-        type="text"
-        id="type"
-        label="Type"
-        onChange={onChange}
-        value={productData.type}
-      />
-      <FormItem
-        placeholder="e.g. sc#####"
-        type="text"
-        id="styleNumber"
-        label="Style Number"
-        onChange={onChange}
-        value={productData.styleNumber}
-      />
-      <FormItem
-        placeholder="e.g. $###.##"
-        className={styles.price}
-        type="decimal"
-        id="price"
-        label="Price"
-        onChange={onChange}
-        value={productData.price}
-      />
-      <FormItem
-        type="text"
-        id="material"
-        label="Material"
-        onChange={onChange}
-        value={productData.material}
-      />
-      <FormItem
-        placeholder="e.g. ABC-EFG-HIGK"
-        type="text"
-        id="sku"
-        label="SKU"
-        onChange={onChange}
-        value={productData.sku}
-      />
-      <FormItem
-        placeholder="e.g. ##"
-        type="numeric"
-        id="quantity"
-        label="Quantity"
-        onChange={onChange}
-        value={productData.quantity}
-      />
-      <FormItemDropdown
-        id="demographics"
-        label="Demographics"
-        onChange={onChange}
-        value={productData.demographics}
-        options={demographics}
-      />
-      <FormItem
-        placeholder="e.g. po-ABCDEFG"
-        type="text"
-        id="globalProductCode"
-        label="Global Product Code"
-        onChange={onChange}
-        value={productData.globalProductCode}
-      />
-      <FormItemDropdown
-        id="active"
-        label="Active"
-        onChange={onChange}
-        value={productData.active}
-        options={activeStatus}
-      />
-
-      <FormItemDropdown
-        id="primaryColorCode"
-        label="Primary Color"
-        onChange={onChange}
-        value={productData.primaryColorCode}
-        options={colors}
-      />
-      <FormItem
-        placeholder="e.g. mm/dd/yyyy"
-        type="text"
-        id="releaseDate"
-        label="Release Date"
-        onChange={onChange}
-        value={productData.releaseDate}
-      />
-
-      <FormItem
-        type="text"
-        id="brand"
-        label="Brand"
-        onChange={onChange}
-        value={productData.brand}
-      />
-
-      <FormItemDropdown
-        id="secondaryColorCode"
-        label="Secondary Color"
-        onChange={onChange}
-        value={productData.secondaryColorCode}
-        options={colors}
-      />
-
+      <div className={styles.item1}>
+        <FormItem
+          className={styles.item1}
+          placeholder="e.g. Soft Golf Shirt "
+          type="text"
+          id="name"
+          label="Product Name"
+          onChange={onChange}
+          value={productData.name}
+        />
+      </div>
+      <div className={styles.item5}>
+        <FormItem
+          type="text"
+          id="category"
+          label="Category"
+          onChange={onChange}
+          value={productData.category}
+        />
+      </div>
+      <div className={styles.item13}>
+        <FormItem
+          placeholder="e.g. https://m.media-amazon.com/images/I/81zNUlGpqJL._AC_UY550_.jpg"
+          type="text"
+          id="imageSrc"
+          label="Image Source"
+          onChange={onChange}
+          value={productData.imageSrc}
+        />
+      </div>
+      <div className={styles.item2}>
+        <FormItem
+          placeholder="e.g. Golf, Men, Soft"
+          type="text"
+          id="description"
+          label="Product Description"
+          onChange={onChange}
+          value={productData.description}
+        />
+      </div>
+      <div className={styles.item4}>
+        <FormItem
+          type="text"
+          id="type"
+          label="Type"
+          onChange={onChange}
+          value={productData.type}
+        />
+      </div>
+      <div className={styles.item15}>
+        <FormItem
+          placeholder="e.g. sc#####"
+          type="text"
+          id="styleNumber"
+          label="Style Number"
+          onChange={onChange}
+          value={productData.styleNumber}
+        />
+      </div>
+      <div className={styles.item6}>
+        <FormItem
+          placeholder="e.g. $###.##"
+          className={styles.price}
+          type="decimal"
+          id="price"
+          label="Price"
+          onChange={onChange}
+          value={productData.price}
+        />
+      </div>
+      <div className={styles.item12}>
+        <FormItem
+          type="text"
+          id="material"
+          label="Material"
+          onChange={onChange}
+          value={productData.material}
+        />
+      </div>
+      <div className={styles.item16}>
+        <FormItem
+          placeholder="e.g. ABC-EFG-HIGK"
+          type="text"
+          id="sku"
+          label="SKU"
+          onChange={onChange}
+          value={productData.sku}
+        />
+      </div>
+      <div className={styles.item7}>
+        <FormItem
+          placeholder="e.g. ##"
+          type="numeric"
+          id="quantity"
+          label="Quantity"
+          onChange={onChange}
+          value={productData.quantity}
+        />
+      </div>
+      <div className={styles.item9}>
+        <FormItemDropdown
+          id="demographics"
+          label="Demographics"
+          onChange={onChange}
+          value={productData.demographics}
+          options={demographics}
+        />
+      </div>
+      <div className={styles.item17}>
+        <FormItem
+          placeholder="e.g. po-ABCDEFG"
+          type="text"
+          id="globalProductCode"
+          label="Global Product Code"
+          onChange={onChange}
+          value={productData.globalProductCode}
+        />
+      </div>
+      <div className={styles.item8}>
+        <FormItemDropdown
+          id="active"
+          label="Active"
+          onChange={onChange}
+          value={productData.active}
+          options={activeStatus}
+        />
+      </div>
+      <div className={styles.item10}>
+        <FormItemDropdown
+          id="primaryColorCode"
+          label="Primary Color"
+          onChange={onChange}
+          value={productData.primaryColorCode}
+          options={colors}
+        />
+      </div>
+      <div className={styles.item14}>
+        <FormItem
+          placeholder="e.g. mm/dd/yyyy"
+          type="text"
+          id="releaseDate"
+          label="Release Date"
+          onChange={onChange}
+          value={productData.releaseDate}
+        />
+      </div>
+      <div className={styles.item3}>
+        <FormItem
+          type="text"
+          id="brand"
+          label="Brand"
+          onChange={onChange}
+          value={productData.brand}
+        />
+      </div>
+      <div className={styles.item11}>
+        <FormItemDropdown
+          id="secondaryColorCode"
+          label="Secondary Color"
+          onChange={onChange}
+          value={productData.secondaryColorCode}
+          options={colors}
+        />
+      </div>
     </div>
   );
 };
