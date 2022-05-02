c511d09a86450033c441518628cc0d075372dcc6
Dany Plugge <dplugge@catalyte.io>
2022-05-02 12:21:26 -0400

    google info

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,6 +5,6 @@ module.exports = Object.freeze({
   PLACEHOLDER_IMAGE: 'https://www.signfix.com.au/wp-content/uploads/2017/09/placeholder-600x400.png',
   PURCHASE_ENDPOINT: '/purchases',
   PRODUCTS_ACTIVE_ENDPOINT: '/products/active',
-  GOOGLE_CLIENT_ID: '518491329427-8ig1j2q680uoghj13uqkq7efbitkvle6.apps.googleusercontent.com', // ENTER CLIENT ID HERE
+  GOOGLE_CLIENT_ID: '201856258433-i0jkaa7khiljea3ngspjjm4ctngv4d4o.apps.googleusercontent.com', // ENTER CLIENT ID HERE
   PRODUCT_ENDPOINT: '/products'
 });
