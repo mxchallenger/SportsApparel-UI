cb604b1ae9107ad9a23d77d80229a70301bd52fc
kmedeiros <kmedeiros@catalyte.io>
2022-05-16 11:58:18 -0500

    updates revieworderwidgetservice http helper

--- src/components/checkout-page/ReviewOrderWidgetService.js
+++ src/components/checkout-page/ReviewOrderWidgetService.js
@@ -23,6 +23,7 @@ export const getSubtotal = (products) => toPrice(products.reduce(
  * @name fetchRate
  * @description Utilizes HttpHelper to make a get request to an API
  * @param {*} setRate sets state for rate
+ * @param {*} ShippingState state for which shipping rate is being requested
  * @param {*} setApiError sets error if response other than 200 is returned
  * @returns sets state for products if 200 response, else sets state for apiError
  */
