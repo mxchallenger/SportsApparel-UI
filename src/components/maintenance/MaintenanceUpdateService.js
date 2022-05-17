58970e1582433b840a2108c8764708c51dfca5e0
Edward Waters <ewaters@catalyte.io>
2022-05-17 01:14:25 -0400

    success toast message change for clarity and function description cleanup

--- src/components/maintenance/MaintenanceUpdateService.js
+++ src/components/maintenance/MaintenanceUpdateService.js
@@ -7,15 +7,14 @@ import Constants from '../../utils/constants';
  * @name updateProducts
  * @description Utilizes HttpHelper to make a Put request to an API
  * @param {*} setUpdatedRow sets state for edited product row
- * @param {*} setApiError sets error if response other than 200 is returned
- * @param {*} updatedRow payload object to be sent in request
- * @returns sets state for products if 200 response, else sets state for apiError
+ * @var updatedRow.data payload object to be sent in our request
+ * @returns toasts confirming a successful or unsuccessful update.
  */
 export default async function updateProducts(setUpdatedRow) {
   await HttpHelper(`${Constants.UPDATE_PRODUCT_ENDPOINT}/${setUpdatedRow.data.id}`, 'PUT', setUpdatedRow.data)
     .then((response) => {
       if (response.ok) {
-        toast.success(`The product with ID of :${setUpdatedRow.data.id} was successfully edited!`);
+        toast.success(`The product with ID of : ${setUpdatedRow.data.id} was successfully updated to the database!`);
         response.json();
       } else {
         toast.error('The product was not added to the database');
