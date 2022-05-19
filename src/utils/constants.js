d0bc8202d82e7665f3b94919ebe0a9fcdbd07b54
Julius Amaker <jamaker@catalyte.io>
2022-05-19 12:45:56 -0700

    changed base url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 module.exports = Object.freeze({
   API_ERROR: 'Oops, something went wrong',
-  BASE_URL_API: 'http://localhost:8085',
-  // BASE_URL_API: 'https://all-american-regex-api.herokuapp.com',
+  // BASE_URL_API: 'http://localhost:8085',
+  BASE_URL_API: 'https://all-american-regex-api.herokuapp.com',
   PLACEHOLDER_IMAGE: 'https://www.signfix.com.au/wp-content/uploads/2017/09/placeholder-600x400.png',
   PURCHASE_ENDPOINT: '/purchases',
   PRODUCTS_FILTER_ENDPOINT: '/products/filters?pageNumber=',
